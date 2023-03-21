03e7b4fce66b91ac99f5fe89994c3f49d0672c02
Emmydrm <emmypee222@gmail.com>
2023-03-21 07:18:59 -0700

    0-fizzbuzz.py

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """ FizzBuzz
 """
-# There seems to be an error in your implementation of the FizzBuzz function. 
-# Specifically, in the second elif statement, you're checking for multiples of 3 instead of multiples of both 3 and 5. 
-# Here's the corrected version of the fizzbuzz function:
 import sys
+
 
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
+    
     - For multiples of three print "Fizz" instead of the number and for
     multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
@@ -26,7 +25,7 @@ def fizzbuzz(n):
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
-        print(" ".join(tmp_result))
+    print(" ".join(tmp_result))
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
